43939bd2e17f86a09115b101ec994fdef17ac92e
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 10:50:37 +0545

    MealsComponents

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,7 +1,11 @@
+import {Fragment} from 'react';
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
 const Meals=()=>{
-
+    <Fragment>
+        <MealsSummary/>
+        <AvailableMeals/> 
+    </Fragment>
 }
 
 export default Meals;
