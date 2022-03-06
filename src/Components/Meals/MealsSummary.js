e6594afbd19a9c393e2a1f83f392717197ebec68
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 09:34:13 +0545

    MealsSummary Component

---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+const MealsSummary=()=>{
+    return(
+        <section>
+            <h2>Delicious Food,Delivered to You</h2>
+            <p>Choose your favourite meal from our broad selection of available meals 
+            and enjoy a delicious lunch or dinner at home.</p>
+            <p>
+                All our meals are cooked with high-quality ingredients.
+            </p>
+        </section>
+    );
+}
+
+export default MealsSummary;
