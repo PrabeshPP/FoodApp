080c161bef6a57d4e8a6024a7e83dfc160403c3c
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 11:36:10 +0545

    MealItem Form is Remaining

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,6 +1,11 @@
-import classes from './Cardmodule.css';
+import classes from './Card.module.css';
 
 const Card=(props)=>{
+    return(
+        <div className={classes.card}>
+            {props.children}
+        </div>
+    )
     
 }
 
