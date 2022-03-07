84c08a9b2b1639916d6bef9c321fe858cac85dc9
Prabesh <prabesh.bista369@gmail.com>
2022-03-07 09:52:08 +0545

    impl. cart close logic

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,20 +1,26 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart=(props)=>{
-    const CartItems=(<ul>
-        {[].map((item)=><li>{item.name}</li>)}
+    const CartItems=(<ul className={classes['cart-items']}>
+        {[{
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    }].map((item)=><li>{item.name}</li>)}
     </ul>);
-    return <div>
+    return <Modal onDismiss={props.onDismiss}>
         {CartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.99</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onDismiss}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 
 }
 
--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,6 +1,29 @@
-import classes
+import classes from './Modal.module.css';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+const BackDrop=(props)=>{
+    return <div className={classes.backdrop} onClick={props.onDismiss}>
+
+    </div>
+
+}
+
+const ModalOverlay=props=>{
+    return <div className={classes.modal} >
+        <div className={classes.content}>{props.children}</div>
+    </div>
+
+}
+
+const portalElem=document.getElementById('div1');
 
 const Modal=(props)=>{
+    return <React.Fragment>
+        {ReactDom.createPortal(<BackDrop onDismiss={props.onDismiss}/>,portalElem)}
+        {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElem)}
+    </React.Fragment>
+   
 
 }
 
