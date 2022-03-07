6355f7786ecb35493b367bf8f2d14cd74e50c2d0
Prabesh <prabesh.bista369@gmail.com>
2022-03-07 11:55:40 +0545

    CartContext

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,14 +1,16 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
-
+import React,{useContext} from "react";
+import CartContext from "../../store/cart-context";
 const HeaderCartButton=(props)=>{
+    const total=useContext(CartContext);
     return (
         <button className={classes.button} onClick={props.onClick}>
          <span className={classes.icon}>
             <CartIcon/>
          </span>
          <span>Basket</span>
-         <span className={classes.badge}>2</span>
+         <span className={classes.badge}>{total.item.length}</span>
 
         </button>
        
