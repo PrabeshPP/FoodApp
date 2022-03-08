08d4360c049b4e9ef6fcab3d4982cd7747727cdc
Prabesh <prabesh.bista369@gmail.com>
2022-03-08 10:24:52 +0545

    remove and add functionality is yet to add in the cart modal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -19,11 +19,11 @@ const defaultCartState = {
       if(existingCartItem){
          const updatedItem={
             ...existingCartItem,
-            amount: parseInt(+existingCartItem.amount + +action.item.amount)
+            amount: +existingCartItem.amount + +action.item.amount
           }
           updatedItems=[...state.items];
           updatedItems[existingCartItemIndex]=updatedItem;
-          console.log(updatedItem.amount)
+          
           
         
       }else{
