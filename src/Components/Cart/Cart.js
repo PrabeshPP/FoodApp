01fee8e080b3b8535a6bef89496b4e13e28c27ff
Prabesh <prabesh.bista369@gmail.com>
2022-03-08 10:16:29 +0545

    Added the Cart func.

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,13 +2,31 @@ import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 
 const Cart=(props)=>{
     const cartCtx=useContext(CartContext);
     const totalAmount=(`$${(cartCtx.totalAmount).toFixed(2)}`)
+
+
+    const cartItemRemoveHandler=(id)=>{
+
+    }
+
+    const cartItemAddHandler=(item)=>{
+
+
+    }
+
     const CartItems=(<ul className={classes['cart-items']}>
-        {cartCtx.items.map((item)=><li>{item.name}</li>)}
+        {cartCtx.items.map((item)=><CartItem key={item.id} 
+        name={item.name}
+        price={item.price}
+        amount={item.amount} 
+        onAdd={cartItemAddHandler.bind(null,item.id)}
+        onRemove={cartItemRemoveHandler.bind(null,item)}
+        />)}
     </ul>);
     return <Modal onDismiss={props.onDismiss}>
         {CartItems}
